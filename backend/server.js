df33c82c64ec21c26e3b9e5a02659f2b185d552d
nkongejr <nkongejr777@gmail.com>
2026-08-12 22:13:27 +0300

    WED

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const app = require('./src/app');
 const connectDB = require('./src/config/db');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 const start = async () => {
     console.log(process.env.MONGO_URI);
